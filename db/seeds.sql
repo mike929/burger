022bf07a08760ced6354ea8ae7f5bde1770a29b8
mike929 <mike929@gmail.com>
2019-03-02 19:45:09 -0500

    working on db

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Select burgers_db
+USE burgers_db;
+
+-- insert 3 burgers
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Beef', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Shirmp', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Kobe', false, CURRENT_TIMESTAMP);
